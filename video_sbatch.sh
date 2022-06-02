3811ffba22486fceec80481556eb4646939faa89
jtgrassel3044 <jgrassel@asu.edu>
2022-06-02 10:56:03 -0600

    video sbatch test

--- video_sbatch.sh
+++ video_sbatch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #SBATCH -N 1
-#SBATCH -n 1
-#SBATCH -t 0-00:01:00
+#SBATCH -n 28
+#SBATCH -t 0-00:10:00
 #SBATCH -p serial
 #SBATCH -q normal
 #SBATCH -o slurm.%j.out
@@ -15,3 +15,4 @@ module purge
 
 module load python/3.7.1
 
+python video_gbvs.py
