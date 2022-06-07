1df059c874a3be7c2a23fde3160ee24add85e553
jtgrassel3044 <jgrassel@asu.edu>
2022-06-07 09:25:26 -0600

    env variable test

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 import os
 
 i = int(os.environ["SLURM_ARRAY_TASK_ID"])
-j = int(os.environ["export_var"])
+j = int(os.environ["export_var1"])
+k = int(os.environ["export_var2"])
 
 print('My task id: ' + str(i))
 print('My Export Var: ' + str(j))
-print('My CPU Count: ' + str(os.cpu_count()))
+print(os.environ)
--- test_sbatch.sh
+++ test_sbatch.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -c 1
+#SBATCH -c 2
 #SBATCH -t 0-00:01:00
 #SBATCH -p serial
 #SBATCH -q normal
@@ -11,7 +11,7 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jgrassel@asu.edu
 #SBATCH --array 1-2
-#SBATCH --export=export_var='299'
+#SBATCH --export=export_var1='199',export_var2='299'
 
 module purge
 
