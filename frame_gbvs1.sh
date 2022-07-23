3eb0e7c3130036b1124e5bfa84bf9ab33227f352
jtgrassel3044 <jgrassel@asu.edu>
2022-07-23 16:41:37 -0600

    prep for T21118 loop 2

---
+++ frame_gbvs1.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -c 28
+#SBATCH -t 0-24:00:00
+#SBATCH -p parallel
+#SBATCH -q normal
+#SBATCH -o slurm.%j.out
+#SBATCH -e slurm.%j.err
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=jgrassel@asu.edu
+#SBATCH --export=image_in_dir='/scratch/jgrassel/image_in1/',gbvs_out_dir='/scratch/jgrassel/gbvs_out1/'
+
+module purge
+
+module load python/3.7.1
+
+python frame_gbvs.py
